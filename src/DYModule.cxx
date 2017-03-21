9847cd943b9634708d509b761927a336052cd4a8
abenecke <anna.benecke@desy.de>
2017-03-21 09:54:49 +0100

    add histograms before JEC_raw factor is applied

--- src/DYModule.cxx
+++ src/DYModule.cxx
@@ -61,6 +61,56 @@ namespace uhh2examples {
     std::unique_ptr<uhh2::Hists> uncorrected_h_jet;
     std::unique_ptr<uhh2::Hists> uncorrected_h_topjet;
 
+    ////////////////////////// uncorrected ///////////////////////
+    //default histogramms
+    std::unique_ptr<uhh2::Hists> uncorrected_input_h_jet;
+    std::unique_ptr<uhh2::Hists>uncorrected_input_h_topjet;
+    std::unique_ptr<uhh2::Hists>uncorrected_input_h_event;
+    std::unique_ptr<uhh2::Hists>uncorrected_input_h_muon;
+    std::unique_ptr<uhh2::Hists>uncorrected_input_h_electron;
+
+
+    std::unique_ptr<PuppiJetHists> uncorrected_h_jet_input; 
+    std::unique_ptr<PuppiTopJetHists> uncorrected_h_topjet_input;
+    std::unique_ptr<DYHists> uncorrected_h_DY_input;
+
+    //Zptk50 hist after selection
+    std::unique_ptr<uhh2::Hists> uncorrected_Zptk50_h_jet;
+    std::unique_ptr<uhh2::Hists>uncorrected_Zptk50_h_topjet;
+    std::unique_ptr<uhh2::Hists>uncorrected_Zptk50_h_event;
+    std::unique_ptr<uhh2::Hists>uncorrected_Zptk50_h_muon;
+    std::unique_ptr<uhh2::Hists>uncorrected_Zptk50_h_electron;
+
+
+    std::unique_ptr<PuppiJetHists> uncorrected_h_jet_Zptk50; 
+    std::unique_ptr<PuppiTopJetHists>uncorrected_h_topjet_Zptk50;
+    std::unique_ptr<DYHists> uncorrected_h_DY_Zptk50;
+ 
+    //Zptg50 hist after selection
+    std::unique_ptr<uhh2::Hists> uncorrected_Zptg50_h_jet;
+    std::unique_ptr<uhh2::Hists>uncorrected_Zptg50_h_topjet;
+    std::unique_ptr<uhh2::Hists>uncorrected_Zptg50_h_event;
+    std::unique_ptr<uhh2::Hists>uncorrected_Zptg50_h_muon;
+    std::unique_ptr<uhh2::Hists>uncorrected_Zptg50_h_electron;
+
+
+    std::unique_ptr<PuppiJetHists> uncorrected_h_jet_Zptg50; 
+    std::unique_ptr<PuppiTopJetHists>uncorrected_h_topjet_Zptg50;
+    std::unique_ptr<DYHists> uncorrected_h_DY_Zptg50;
+
+    //output hist after selection
+    std::unique_ptr<uhh2::Hists> uncorrected_output_h_jet;
+    std::unique_ptr<uhh2::Hists>uncorrected_output_h_topjet;
+    std::unique_ptr<uhh2::Hists>uncorrected_output_h_event;
+    std::unique_ptr<uhh2::Hists>uncorrected_output_h_muon;
+    std::unique_ptr<uhh2::Hists>uncorrected_output_h_electron;
+
+
+    std::unique_ptr<PuppiJetHists> uncorrected_h_jet_output; 
+    std::unique_ptr<PuppiTopJetHists> uncorrected_h_topjet_output;
+    std::unique_ptr<DYHists> uncorrected_h_DY_output;
+
+    ////////////////////////// corrected ///////////////////////
     //default histogramms
     std::unique_ptr<uhh2::Hists> input_h_jet;
     std::unique_ptr<uhh2::Hists>input_h_topjet;
@@ -150,12 +200,63 @@ namespace uhh2examples {
     Zptk50_sel.reset(new ZptCut(50,"max"));
     Zptg50_sel.reset(new ZptCut(50,"min"));
 
- ///////////////////////////    Hists    /////////////////////////////////  
+    ///////////////////////////    Hists    /////////////////////////////////  
 
     uncorrected_h_jet.reset(new JetHists     (ctx, "uncorrected_Jets"));
     if(ctx.get("mode")=="Puppi")  uncorrected_h_topjet.reset(new JetHists (ctx, "uncorrected_TopJets",4,"patJetsAK8PFPUPPI"));
     else uncorrected_h_topjet.reset(new JetHists (ctx, "uncorrected_TopJets",4,"patJetsAK8PFCHS"));
 
+    ////////////////// uncorrected  ///////////////////////////
+    uncorrected_input_h_jet.reset(new JetHists     (ctx, "uncorrected_input_Jets"));
+    uncorrected_input_h_topjet.reset(new JetHists (ctx, "uncorrected_input_TopJets",4,"patJetsAK8PFCHS"));
+    uncorrected_input_h_event.reset(new EventHists (ctx, "uncorrected_input_Event"));
+    uncorrected_input_h_muon.reset(new MuonHists (ctx, "uncorrected_input_Muon"));
+    uncorrected_input_h_electron.reset(new ElectronHists (ctx, "uncorrected_input_Electron"));
+
+   
+    uncorrected_h_jet_input.reset(new PuppiJetHists(ctx, "uncorrected_jet_input"));
+    uncorrected_h_topjet_input.reset(new PuppiTopJetHists(ctx, "uncorrected_topjet_input"));
+    uncorrected_h_DY_input.reset(new DYHists(ctx, "uncorrected_DY_input"));
+
+    //Zptk50 hist after selection
+    uncorrected_Zptk50_h_jet.reset(new JetHists     (ctx, "uncorrected_Zptk50_Jets"));
+    uncorrected_Zptk50_h_topjet.reset(new JetHists (ctx, "uncorrected_Zptk50_TopJets",4,"patJetsAK8PFCHS"));
+    uncorrected_Zptk50_h_event.reset(new EventHists (ctx, "uncorrected_Zptk50_Event"));
+    uncorrected_Zptk50_h_muon.reset(new MuonHists (ctx, "uncorrected_Zptk50_Muon"));
+    uncorrected_Zptk50_h_electron.reset(new ElectronHists (ctx, "Zptk50_Electron"));
+
+   
+    uncorrected_h_jet_Zptk50.reset(new PuppiJetHists(ctx, "uncorrected_jet_Zptk50"));
+    uncorrected_h_topjet_Zptk50.reset(new PuppiTopJetHists(ctx, "uncorrected_topjet_Zptk50"));
+    uncorrected_h_DY_Zptk50.reset(new DYHists(ctx, "uncorrected_DY_Zptk50"));
+
+    //Zptg50 hist after selection
+    uncorrected_Zptg50_h_jet.reset(new JetHists     (ctx, "uncorrected_Zptg50_Jets"));
+    uncorrected_Zptg50_h_topjet.reset(new JetHists (ctx, "uncorrected_Zptg50_TopJets",4,"patJetsAK8PFCHS"));
+    uncorrected_Zptg50_h_event.reset(new EventHists (ctx, "uncorrected_Zptg50_Event"));
+    uncorrected_Zptg50_h_muon.reset(new MuonHists (ctx, "uncorrected_Zptg50_Muon"));
+    uncorrected_Zptg50_h_electron.reset(new ElectronHists (ctx, "uncorrected_Zptg50_Electron"));
+
+   
+    uncorrected_h_jet_Zptg50.reset(new PuppiJetHists(ctx, "uncorrected_jet_Zptg50"));
+    uncorrected_h_topjet_Zptg50.reset(new PuppiTopJetHists(ctx, "uncorrected_topjet_Zptg50"));
+    uncorrected_h_DY_Zptg50.reset(new DYHists(ctx, "uncorrected_DY_Zptg50"));
+
+
+    //output hist after selection
+    uncorrected_output_h_jet.reset(new JetHists     (ctx, "uncorrected_output_Jets"));
+    uncorrected_output_h_topjet.reset(new JetHists (ctx, "uncorrected_output_TopJets",4,"patJetsAK8PFCHS"));
+    uncorrected_output_h_event.reset(new EventHists (ctx, "uncorrected_output_Event"));
+    uncorrected_output_h_muon.reset(new MuonHists (ctx, "uncorrected_output_Muon"));
+    uncorrected_output_h_electron.reset(new ElectronHists (ctx, "uncorrected_output_Electron"));
+
+   
+    uncorrected_h_jet_output.reset(new PuppiJetHists(ctx, "uncorrected_jet_output"));
+    uncorrected_h_topjet_output.reset(new PuppiTopJetHists(ctx, "uncorrected_topjet_output"));
+    uncorrected_h_DY_output.reset(new DYHists(ctx, "uncorrected_DY_output"));
+
+
+    /////////////////////////////////////  corrected  ///////////////////////////
     input_h_jet.reset(new JetHists     (ctx, "input_Jets"));
     input_h_topjet.reset(new JetHists (ctx, "input_TopJets",4,"myAK8jets_uncorrected"));
     input_h_event.reset(new EventHists (ctx, "input_Event"));
@@ -216,8 +317,79 @@ namespace uhh2examples {
     uncorrected_h_jet   ->fill(event);
     uncorrected_h_topjet ->fill(event);
 
+    uncorrected_input_h_jet   ->fill(event);
+    uncorrected_input_h_event   ->fill(event);
+    uncorrected_input_h_topjet ->fill(event);
+    uncorrected_input_h_muon   ->fill(event);
+    uncorrected_input_h_electron ->fill(event);
+
+
+    uncorrected_h_jet_input->fill(event);
+    uncorrected_h_topjet_input->fill(event);
+    uncorrected_h_DY_input->fill(event);
+
+    if(berror)  std::cout<<"-------- DYModul::Selection -------"<<std::endl;
+    ///////////////////////////////////////////////////          Selection    //////////////////////////////////////////
+    if(channel=="muon")muo_cleaner->process(event);
+    else ele_cleaner->process(event);
+    bool pass_lep1 = lep1_sel->passes(event);
+    if(!pass_lep1)return false;
+
+    bool pass_ZmassCut = Zmass_sel->passes(event);
+    if(!pass_ZmassCut)return false;
+
+
+    if(berror)  std::cout<<"-------- DYModul::ZPt <50 -------"<<std::endl;
+    ///////////////////////////////////////////////////          ZPt <50     //////////////////////////////////////////
+    bool pass_Zptk50 = Zptk50_sel->passes(event);
+    if(pass_Zptk50){
+    //Zptk50 hist after selection
+    uncorrected_Zptk50_h_jet   ->fill(event);
+    uncorrected_Zptk50_h_event   ->fill(event);
+    uncorrected_Zptk50_h_topjet ->fill(event);
+    uncorrected_Zptk50_h_muon   ->fill(event);
+    uncorrected_Zptk50_h_electron ->fill(event);
+
+
+    uncorrected_h_jet_Zptk50->fill(event);
+    uncorrected_h_topjet_Zptk50->fill(event);
+    uncorrected_h_DY_Zptk50->fill(event);
+    }
+
+    if(berror)  std::cout<<"-------- DYModul::ZPt >50 -------"<<std::endl;
+    ///////////////////////////////////////////////////          ZPt >50     //////////////////////////////////////////
+    bool pass_Zptg50 = Zptg50_sel->passes(event);
+    if(pass_Zptg50){
+    //Zptg50 hist after selection
+    uncorrected_Zptg50_h_jet   ->fill(event);
+    uncorrected_Zptg50_h_event   ->fill(event);
+    uncorrected_Zptg50_h_topjet ->fill(event);
+    uncorrected_Zptg50_h_muon   ->fill(event);
+    uncorrected_Zptg50_h_electron ->fill(event);
+
+
+    uncorrected_h_jet_Zptg50->fill(event);
+    uncorrected_h_topjet_Zptg50->fill(event);
+    uncorrected_h_DY_Zptg50->fill(event);
+    }
+
+    if(berror)  std::cout<<"-------- DYModul::Output -------"<<std::endl;
+    ///////////////////////////////////////////////////          output     //////////////////////////////////////////
+    //output hist after selection
+    uncorrected_output_h_jet   ->fill(event);
+    uncorrected_output_h_event   ->fill(event);
+    uncorrected_output_h_topjet ->fill(event);
+    uncorrected_output_h_muon   ->fill(event);
+    uncorrected_output_h_electron ->fill(event);
+
+
+    uncorrected_h_jet_output->fill(event);
+    uncorrected_h_topjet_output->fill(event);
+    uncorrected_h_DY_output->fill(event);
+
+
     if(berror)  std::cout<<"-------- DYModul::Jec_factor_raw -------"<<std::endl;
-    ///////////////////////////   Jets and topjets back correcting  ///////////////////////////////// 
+    ////////////////////////////////////////////////////////////////////////////////////   Jets and topjets back correcting  ///////////////////////////////////////////////////////////////////////////////////////// 
     for (auto & jet:*event.jets)  jet.set_v4(jet.v4() * jet.JEC_factor_raw() );
     if(berror) std::cout<<"myAK8jets first jet mass  "<<myAK8jets.at(0).v4().M()<<std::endl;
     for (auto & jet:myAK8jets)   jet.set_v4(jet.v4() * jet.JEC_factor_raw() );
@@ -240,16 +412,16 @@ namespace uhh2examples {
     ///////////////////////////////////////////////////          Selection    //////////////////////////////////////////
     if(channel=="muon")muo_cleaner->process(event);
     else ele_cleaner->process(event);
-    bool pass_lep1 = lep1_sel->passes(event);
+    pass_lep1 = lep1_sel->passes(event);
     if(!pass_lep1)return false;
 
-    bool pass_ZmassCut = Zmass_sel->passes(event);
+    pass_ZmassCut = Zmass_sel->passes(event);
     if(!pass_ZmassCut)return false;
 
 
     if(berror)  std::cout<<"-------- DYModul::ZPt <50 -------"<<std::endl;
     ///////////////////////////////////////////////////          ZPt <50     //////////////////////////////////////////
-    bool pass_Zptk50 = Zptk50_sel->passes(event);
+    pass_Zptk50 = Zptk50_sel->passes(event);
     if(pass_Zptk50){
     //Zptk50 hist after selection
     Zptk50_h_jet   ->fill(event);
@@ -266,7 +438,7 @@ namespace uhh2examples {
 
     if(berror)  std::cout<<"-------- DYModul::ZPt >50 -------"<<std::endl;
     ///////////////////////////////////////////////////          ZPt >50     //////////////////////////////////////////
-    bool pass_Zptg50 = Zptg50_sel->passes(event);
+    pass_Zptg50 = Zptg50_sel->passes(event);
     if(pass_Zptg50){
     //Zptg50 hist after selection
     Zptg50_h_jet   ->fill(event);
